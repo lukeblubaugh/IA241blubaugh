bf831631783e5333d11187d80df39fbde5a741ea
lukeblubaugh <122937245+lukeblubaugh@users.noreply.github.com>
2023-01-26 16:59:30 -0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,43 @@
+'''
+lab 2
+numbers variables and strings
+'''
+
+#6.1
+my_name = 'Tom'
+print(my_name.upper())
+
+#6.2
+my_id = 123
+print(my_id)
+
+#6.3
+#123 = my_id
+
+#6.4
+my_id = your_id = 123
+print(your_id)
+
+#6.5
+my_id_str = '123'
+print(my_id_str)
+
+#6.6
+#my_name + my_id
+
+#6.7
+my_name + my_id_str
+print(my_name + my_id_str)
+
+#6.8
+my_name*3
+print(my_name*3)
+
+#6.9
+print('hello world. This is my first python string.' .split('.'))
+
+#6.10
+message = 'Toms id is 123'
+print(message)
+message = "Tom's id is 123"
+print(message)
