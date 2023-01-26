f5d69b00dd27feeee152388f78adf65f23252c37
lukeblubaugh <122937245+lukeblubaugh@users.noreply.github.com>
2023-01-26 17:00:07 -0500

    Add files via upload

---
+++ lec2.py
@@ -0,0 +1,31 @@
+'''
+lec 2
+numbers, strings, and variables
+'''
+'''
+print( type('hello'))
+
+print( type(123))
+
+print( type(123.123))
+
+print( type('567.2'))
+
+print( "Tom's cat")
+print( "it's our second python class")
+'''
+#print( 'Hello World')
+
+#print( 'Hello World' .upper())
+
+#print( '   Hello World ' .strip())
+
+#print( 'hello world'.split()) #default split by spaces
+#print( 'hellow world'.split('o')) #split by 'o'
+
+#print( ' the & cow & was & blue'.split('&'))
+#print('hello '+'world')
+
+my_num = 6143594292
+print(my_num)
+
