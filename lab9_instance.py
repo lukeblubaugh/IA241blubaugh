6ee9e011620fc20ea7d5db4a2acb0625616d0e90
lukeblubaugh <122937245+lukeblubaugh@users.noreply.github.com>
2023-03-23 16:49:54 -0400

    Add files via upload

---
+++ lab9_instance.py
@@ -0,0 +1,20 @@
+#lab9 instance
+#define class
+
+from lab9 import my_stat
+
+my_cal_instance = my_stat()
+
+print(my_cal_instance.cal_sigma(3,5))
+
+print(my_cal_instance.cal_pi(3,5))
+
+print(my_cal_instance.cal_fact(5))
+
+print(my_cal_instance.cal_perm(5,2))
+
+import numpy
+print(numpy)
+
+print(numpy.math.factorial (999))
+print(my_cal_instance.cal_fact(999))
